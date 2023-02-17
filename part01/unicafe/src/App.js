6114547f5f9286f220cb5e13954d31433c4a6eda
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-17 16:52:11 +0100

    part01: Complete exercise 1.11

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -10,10 +10,10 @@ const Button = ({ value, onClick }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <>
-      {text} {value}
-      <br />
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -34,13 +34,15 @@ const Statistics = ({
       {all === 0 ? (
         <p>No feedback given</p>
       ) : (
-        <div>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={positive} />
-        </div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={positive} />
+          </tbody>
+        </table>
       )}
     </div>
   )
