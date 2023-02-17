051685abf8c2f2620ed7de33876232828eeeb305
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-17 16:37:38 +0100

    part01: Complete exercise 1.7

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -6,18 +6,32 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [all, setAll] = useState(0)
+
+  const [averageScore, setAverageScore] = useState(0)
+  const average = all === 0 ? '0' : averageScore / all
+
+  const [positiveCount, setPositiveCount] = useState(0)
+  const positive = all === 0 ? '0' : `${(positiveCount / all) * 100} %`
+
   const handleButtonClick = (event) => {
     const value = event.target.value
 
     switch (value) {
       case 'good':
         setGood(good + 1)
+        setAll(all + 1)
+        setAverageScore(averageScore + 1)
+        setPositiveCount(positiveCount + 1)
         break
       case 'neutral':
         setNeutral(neutral + 1)
+        setAll(all + 1)
         break
       case 'bad':
         setBad(bad + 1)
+        setAll(all + 1)
+        setAverageScore(averageScore - 1)
         break
       default:
         break
@@ -45,6 +59,12 @@ const App = () => {
         neutral {neutral}
         <br />
         bad {bad}
+        <br />
+        all {all}
+        <br />
+        average {average}
+        <br />
+        positive {positive}
         <br />
       </div>
     </div>
