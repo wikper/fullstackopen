c90efeaec4373698688553d0c3e7e4ccd5a03041
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-18 20:53:16 +0100

    part03: Complete exercise 3.6

--- part03/phonebook/index.js
+++ part03/phonebook/index.js
@@ -71,6 +71,13 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
+  const { name, number } = body
+  if (!name || !number) {
+    return response.status(400).json({ error: 'name or number is missing' })
+  }
+  if (persons.find((person) => person.name === name)) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
   const person = {
     id: generateId(),
     name: body.name,
