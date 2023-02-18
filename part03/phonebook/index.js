19d08bc91d610f97122265ed20a41b30c70e8b94
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-18 20:56:25 +0100

    part03: Complete exercise 3.8

--- part03/phonebook/index.js
+++ part03/phonebook/index.js
@@ -6,7 +6,11 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {
