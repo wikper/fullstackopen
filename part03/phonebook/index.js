212809977addbd0d8189fc7b1e4bbf365766c6c9
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-19 12:41:09 +0100

    part03: Complete exercise 3.17

--- part03/phonebook/index.js
+++ part03/phonebook/index.js
@@ -109,6 +109,19 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
