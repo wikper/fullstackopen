ad89f7884a97e9112177504880cea46a16e56354
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-19 12:27:54 +0100

    part03: Complete exercise 3.13

--- part03/phonebook/index.js
+++ part03/phonebook/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 const app = express()
 
@@ -56,7 +59,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
