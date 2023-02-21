0bc8c646f63838972d071dc2406a0e40faea1d26
Wiktoria Peretiatkowicz <124917149+wikper@users.noreply.github.com>
2023-02-21 20:34:03 +0100

    part04: Complete exercise 4.5

--- part04/blog-list/utils/list_helper.js
+++ part04/blog-list/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes))
+  const favBlog = blogs.find((blog) => blog.likes === maxLikes)
+  if (!favBlog) {
+    return null
+  }
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes,
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
